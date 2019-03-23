37ce3c5ee72caf9812478fc3e6f45ef7716989e3
Tina Thomas <tinathomasedu@gmail.com>
2019-03-23 02:24:27 -0400

    blood bone knives draft 1
    blood bone knives front-end-design first draft

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function ig_function()
+{
+    var new_tab = window.open("https://www.instagram.com/bloodboneknives/", '_blank');
+    new_tab.focus();
+    /*document.location="https://www.instagram.com/bloodboneknives/";*/
+}
