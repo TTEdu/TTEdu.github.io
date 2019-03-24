89cd0843033ad3ffe3b99b2b6636f1bf1d635107
Tina Thomas <tinathomasedu@gmail.com>
2019-03-24 03:56:20 -0400

    draft2
    Several items to clean up / add comments - think it meets requirements - STILL UNDER CONSTRUCTION

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
+/*************************************
+  Your project includes custom-written 
+  JavaScript including at least one 
+  JavaScript function 
+  (jQuery is fine as well) 
+  located in a .js file.  
+  Exception: using only alert() or 
+  prompt() will 
+  not meet this requirement
+ *************************************/
+
 function ig_function()
 {
     var new_tab = window.open("https://www.instagram.com/bloodboneknives/", '_blank');
     new_tab.focus();
     /*document.location="https://www.instagram.com/bloodboneknives/";*/
 }
+
