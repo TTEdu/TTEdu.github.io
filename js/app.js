5677d073aee5061c58a0d56362bec3a28c7bf932
Tina Thomas <tinathomasedu@gmail.com>
2019-04-01 10:11:22 -0400

    update
    update

--- js/app.js
+++ js/app.js
@@ -1,19 +1,11 @@
 /*************************************
-  Your project includes custom-written 
-  JavaScript including at least one 
-  JavaScript function 
-  (jQuery is fine as well) 
-  located in a .js file.  
-  Exception: using only alert() or 
-  prompt() will 
-  not meet this requirement
+  simple js applying focus to url 
+  in a new tab / window
  *************************************/
 
- /* simple js applying focus to url in a new tab / window */
 function ig_function()
 {
     var new_tab = window.open("https://www.instagram.com/bloodboneknives/", '_blank');
     new_tab.focus();
     /*document.location="https://www.instagram.com/bloodboneknives/";*/
 }
-
